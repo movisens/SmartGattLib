1a61cb8e707f56991636f612fd636322545a4bc7
Ulrich Großmann <grossmann@movisens.com>
2020-05-25 21:27:01 +0200

    added required characteristics to Characteristic

--- src/main/java/com/movisens/smartgattlib/helper/Characteristic.java
+++ src/main/java/com/movisens/smartgattlib/helper/Characteristic.java
@@ -4,13 +4,20 @@ import com.movisens.smartgattlib.attributes.DefaultAttribute;
 
 public class Characteristic<T extends AbstractAttribute> extends UuidObject
 {
-
     private Class<T> attributeClass;
+    
+    Characteristic<?> requiredCharacteristics[];
 
-    public Characteristic(String uuid, String name, Class<T> attributeClass)
+    public Characteristic<?>[] getRequiredCharacteristics()
+    {
+        return requiredCharacteristics;
+    }
+
+    public Characteristic(String uuid, String name, Class<T> attributeClass, Characteristic<?>... requiredCharacteristics)
     {
         super(uuid, name);
-         this.attributeClass = attributeClass;
+        this.attributeClass = attributeClass;
+        this.requiredCharacteristics = requiredCharacteristics;
     }
 
     public AbstractAttribute createAttribute(byte[] data)
