822f5fb7c1831038dc803f9a9e2fd2884a426732
Ulrich Großmann <ulrich.grossmann@movisens.com>
2026-04-23 14:04:21 +0200

    Return scalar sealing key from createKey

--- src/main/java/com/movisens/smartgattlib/security/KeyGenerator.java
+++ src/main/java/com/movisens/smartgattlib/security/KeyGenerator.java
@@ -6,16 +6,11 @@ import com.movisens.smartgattlib.helper.GattByteBuffer;
 
 public class KeyGenerator
 {
-    public static long[] createKey(String password) {
+    public static long createKey(String password) {
 
         SecretKey key = AesUtil.createAesKey(password);
         GattByteBuffer bb = GattByteBuffer.wrap(key.getEncoded());
-
-        long[] result = new long[2];
-        result[0] = bb.getInt64();
-        result[1] = bb.getInt64();
-
-        return result;
+        return bb.getInt64();
     }
     
 }
